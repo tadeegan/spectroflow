2045513917307fd77f8efa3daef2f389838cc86c
Thomas Deegan <tadeegan@gmail.com>
2014-04-02 22:00:05 -0500

    more random

--- particle.js
+++ particle.js
@@ -1,9 +1,9 @@
 var Particle = (function(){
-    var DAMPING = 0.93;
+    var DAMPING = 0.91;
     var Particle = function(startx, starty, endx, endy){
         var _position = new Vector2(startx, starty);
         var _destination = new Vector2(endx, endy);
-        var _velocity = new Vector2(Math.random()*20-10,Math.random()*3-1);
+        var _velocity = new Vector2(Math.random()*10-5,Math.random()*30-15);
         var _self = this;
         var _radius = 10;
         var _reached = false;
@@ -62,7 +62,7 @@ var Particle = (function(){
             return;
         } 
         context.beginPath();
-        context.arc(this.get_position().x, this.get_position().y, 5, 0, 2 * Math.PI, false);
+        context.arc(this.get_position().x, this.get_position().y, 3, 0, 2 * Math.PI, false);
         context.fillStyle = this.get_color();
         context.fill();
     }
