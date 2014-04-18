2ac4d16ec7426bb4eafa4ae5ca5b02e6f7a879f2
Thomas Deegan <tadeegan@gmail.com>
2014-04-17 23:41:09 -0500

    fixing decibel scale and add sound cloud integration

--- spectro_flow.js
+++ spectro_flow.js
@@ -5,12 +5,17 @@ var buckets = [];
 var FPS = 40;
 
 var width = $(window).width() - 10;
-var height = ($(window).height() - 80) * .6;
+var height = ($(document).height() - 80);
 $("#flow-canvas").attr("width", width);
 $("#flow-canvas").attr("height", height);
 
+var url_selector = $("#soundcloud-container");
+url_selector.css("right",($("#flow-canvas").width() - url_selector.width())/2);
+url_selector.css("bottom", ($("#flow-canvas").height() - url_selector.height())/2)
 
-var padding = height / (num_buckets);
+$("#player").css("display","none");
+
+var padding = height / (num_buckets + 1);
 
 //var colors = ['#00FF00','#FFFF00','#FF0000','#FF00FF','#0000FF','#00FFFF']
 var colors = ['#2ECC40','#FFDC00','#FF4136','#0074D9','#7FDBFF'];
@@ -23,21 +28,17 @@ for(var i = 0; i < num_buckets; i++){
 
 var particles = [];
 
+var trim_width = 0;
+
 function start(){
-    console.log("starting!");
     var canvas = document.getElementById('flow-canvas');
     var context = canvas.getContext('2d');
     context.fillStyle    = '#222';  // set text color
 
     clearStartX = 0;
 
-    var canvas2 = document.getElementById('line-canvas');
-    var context2 = canvas2.getContext('2d');
-
-
     setInterval(function(){
-        context.clearRect (clearStartX,0,canvas.width - clearStartX,canvas.height);
-        context2.clearRect (0,0,canvas2.width,canvas2.height);
+        clear_canvas();
 
         var toRemove = 0
         particles.forEach(function(entry) {
@@ -49,15 +50,15 @@ function start(){
           }
           entry.display(context);
         });
-        // clean up array if it gets too large
-        if (toRemove > 300) {
-            particles.splice(0, 200);
+        if (particles.length > 1000) {
+            trim_width = particles[300].get_position().x + 40;
+            particles.splice(0, 300);
         }
     },1000/FPS);
 }
 function clear_canvas(){
     var canvas = document.getElementById('flow-canvas');
     var context = canvas.getContext('2d');
-    context.clearRect (0,0,canvas.width,canvas.height);
+    context.clearRect (trim_width,0,canvas.width,canvas.height);
 }
 start();
